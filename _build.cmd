17bddad0598fe139fa89d586ae5d8ab0b1941e95
Fabio Belavenuto <belavenuto@gmail.com>
2023-12-11 12:41:36 -0300

    Organizing Files, Compiling bitstream with docker

--- _build.cmd
+++ _build.cmd
@@ -8,19 +8,27 @@ echo VER_MAIN	equ	%VER_MAIN% > %cd%/driver/VERSION.INC
 echo VER_SEC	equ	%VER_SEC% >> %cd%/driver/VERSION.INC
 echo VER_REV	equ	%VER_REV% >> %cd%/driver/VERSION.INC
 
+set /p NXT_VERSION=<Nextor\VERSION
+set /p HW_VERSION=<CPLD\VERSION
+
 echo Updating docker image
 docker pull fbelavenuto/8bitcompilers
+docker pull fbelavenuto/xilinxise
 
-echo Building Driver
+echo Building Driver Version %VER_MAIN%.%VER_SEC%.%VER_REV%
 docker run --rm -it -v %cd%/driver:/src fbelavenuto/8bitcompilers N80 DRIVER.ASM DRIVER.BIN --listing-file DRIVER.LST
 IF ERRORLEVEL 1 GOTO error
 
-echo Building ROM
-docker run --rm -it -v %cd%:/src fbelavenuto/8bitcompilers mknexrom Nextor/Nextor-2.1.0.base.dat driver/SDM%VER_MAIN%%VER_SEC%%VER_REV%.ROM /d:driver/DRIVER.BIN /m:Nextor/Mapper.ASCII16.bin
+echo Building ROM with Nextor %NXT_VERSION%
+docker run --rm -it -v %cd%:/src fbelavenuto/8bitcompilers mknexrom Nextor/Nextor-%NXT_VERSION%.base.dat driver/SDXC%VER_MAIN%%VER_SEC%%VER_REV%.ROM /d:driver/DRIVER.BIN /m:Nextor/Mapper.ASCII16.bin
 IF ERRORLEVEL 1 GOTO error
 
 echo Building Updater
 docker run --rm -it -v %cd%/SW:/src fbelavenuto/8bitcompilers make -C Updater
+IF ERRORLEVEL 1 GOTO error
+
+echo Building CPLD bitstream Version %HW_VERSION%
+docker run --rm -it -v %cd%/CPLD:/workdir fbelavenuto/xilinxise make VERSION=%HW_VERSION%
 IF ERRORLEVEL 1 GOTO error
 
 goto ok
--- _build.sh
+++ _build.sh
@@ -9,14 +9,21 @@ echo "VER_MAIN	equ	$VER_MAIN" > $PWD/driver/VERSION.INC
 echo "VER_SEC	equ	$VER_SEC" >> $PWD/driver/VERSION.INC
 echo "VER_REV	equ	$VER_REV" >> $PWD/driver/VERSION.INC
 
+NXT_VERSION=`<Nextor/VERSION`
+HW_VERSION=`<CPLD\VERSION`
+
 echo Updating docker image
 docker pull fbelavenuto/8bitcompilers
+docker pull fbelavenuto/xilinxise
 
-echo Building Driver
+echo Building Driver Version ${VER_MAIN}.${VER_SEC}.${VER_REV}
 docker run --rm -it -v $PWD/driver:/src fbelavenuto/8bitcompilers N80 DRIVER.ASM DRIVER.BIN --listing-file DRIVER.LST
 
-echo Building ROM
-docker run --rm -it -v $PWD:/src fbelavenuto/8bitcompilers mknexrom Nextor/Nextor-2.1.0.base.dat driver/SDM${VER_MAIN}${VER_SEC}${VER_REV}.ROM /d:driver/driver.bin /m:Nextor/Mapper.ASCII16.bin
+echo Building ROM with Nextor ${NXT_VERSION}
+docker run --rm -it -v $PWD:/src fbelavenuto/8bitcompilers mknexrom Nextor/Nextor-${NXT_VERSION}.base.dat driver/SDXC${VER_MAIN}${VER_SEC}${VER_REV}.ROM /d:driver/driver.bin /m:Nextor/Mapper.ASCII16.bin
 
 echo Building Updater
 docker run --rm -it -v $PWD/SW:/src fbelavenuto/8bitcompilers make -C Updater
+
+echo Building CPLD bitstream Version ${HW_VERSION}
+docker run --rm -it -v $PWD/CPLD:/workdir fbelavenuto/xilinxise make VERSION=${HW_VERSION}
